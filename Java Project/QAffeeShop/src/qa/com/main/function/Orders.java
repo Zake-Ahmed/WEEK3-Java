c6aa630e3a41a095480f5ef37603db89a9098e98
Zake-Ahmed <zake3012@gmail.com>
2022-05-11 16:33:18 +0100

    End of Day3

---
+++ Java Project/QAffeeShop/src/qa/com/main/function/Orders.java
@@ -0,0 +1,24 @@
+package qa.com.main.function;
+
+public class Orders {
+	public int orderNum;
+	public String drink;
+	public String meal;
+	public float price;
+	public boolean takeway;
+	
+	
+	public Orders(int orderNum, String drink, String meal, float price, boolean takeway) {
+		
+		this.orderNum = orderNum;
+		this.drink = drink;
+		this.meal = meal;
+		this.price = price;
+		this.takeway = takeway;
+	}
+	
+	
+	
+	
+
+}
